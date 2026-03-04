79b132453910db7d29593d7a2746d4c794b99555
Louis Abery <louis@abery.com>
2026-03-04 16:01:22 +0000

    asked questions

--- extract_board_LA.py
+++ extract_board_LA.py
@@ -1,5 +1,8 @@
 import json
 
+import requests
+
+
 def load_candidates(filepath):
     with open(filepath, 'r') as f:
         # Based on your snippet, this is likely a list of objects
@@ -21,5 +24,51 @@ remaining = all_candidates
 print(f"Starting with {len(remaining)} candidates.")
 
 
-print(remaining[62])
+#print(remaining[62])
 
+# 3. Suppose the API says "The top_color is NOT white"
+# You keep only those where 'top_color' is NOT 'white'
+remaining = [c for c in remaining if c['traits'].get("commute_type") == "bus"]
+print(f"After bus filter: {len(remaining)} left.")
+
+
+
+
+
+
+
+
+
+
+
+
+base_url = "https://guesswhoservice-cjrwkt3ccq-nw.a.run.app"
+session_id = "s_f6f28223"  
+
+
+question_id = "T39"
+
+
+
+
+
+
+def ask_questions2(base_url, session_id):
+    headers = {"questionId": "T39", "value": "bus"}
+
+    response = requests.post(
+        f"{base_url}/sessions/{session_id}/ask",
+        json=headers
+    )
+
+    if response.status_code != 200:
+        print("Error starting session:", response.status_code)
+        print(response.text)
+        return None
+
+    return response.json()
+
+# ASK QUESTIONS HEREERERER
+
+#question_result = ask_questions2(base_url, session_id)
+#print(question_result)
