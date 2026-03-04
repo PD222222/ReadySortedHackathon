075840ebab3cf9815f6de45fe5b0dd3ac4e94657
Louis Abery <louis@abery.com>
2026-03-04 15:11:35 +0000

    session status

---
+++ get_session_status_LA.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+BASE_URL = "https://guesswhoservice-cjrwkt3ccq-nw.a.run.app"
+SESSION = "s_f6f28223"   # <-- PUT YOUR REAL TEAM ID HERE
+
+
+
+def get_session_status(SESSION):
+    headers = {
+        "X-Session-Id": SESSION
+    }
+
+    response = requests.get(
+        f"{BASE_URL}/sessions/{SESSION}/status",
+        headers=headers
+    )
+
+    if response.status_code != 200:
+        print("Error getting session status:", response.status_code)
+        print(response.text)
+        return None
+
+    data = response.json()
+    return data
+
+session_data = get_session_status(SESSION)
+print("session status:", session_data)
